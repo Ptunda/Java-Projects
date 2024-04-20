dc2b072d994393092d1c44232d0d830026ab4927
Ptunda <ptunda@pgh.yearup.org>
2024-04-20 19:37:58 -0400

    replaccd the for lopp wwith a for-each, padded the score with  zeros, & added a Thread.sleep() method to pause the execution for 1000 millisecs

--- grade-calculator/src/main/java/com/pluralsight/GradesCalculator.java
+++ grade-calculator/src/main/java/com/pluralsight/GradesCalculator.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 public class GradesCalculator {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Random random = new Random();
 
@@ -19,14 +19,17 @@ public class GradesCalculator {
         int low = 1;
 
         // iterate through the whole class, computing a random sequence of result for each student
-        for (int i = 0; i < students.length ; i++) {
+        for (String student : students) {
 
             int randomScore = random.nextInt(high - low + 1) + low;
 
+            // check if the score each student gets falls in a particular range and assign them a grade
             char grade = (randomScore >= 80) ? 'A' : (randomScore >= 70) ? 'B' : (randomScore >= 60) ? 'C' : (randomScore >= 50) ? 'D' : (randomScore >= 40) ? 'E' : 'F';
 
             // print every student's score against their grades
-            System.out.printf("Hey %s, your score is %d. so your Grade is %c.\n", students[i], randomScore, grade);
+            System.out.printf("Hey %s, your score is %02d/100. so your Grade is %c.\n", student, randomScore, grade);
+
+            Thread.sleep(1000);
 
         }
 
